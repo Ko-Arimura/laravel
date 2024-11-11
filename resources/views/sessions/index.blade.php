03455ccc0501528d673ac7b9d8eb71f540d5a26f
Ko-Arimura <gyoucun000@gmail.com>
2024-11-11 15:39:06 +0900

    final2

--- resources/views/sessions/index.blade.php
+++ resources/views/sessions/index.blade.php
@@ -9,29 +9,25 @@
 
 <body>
   <h1>ショッピングカート</h1>
-
   @if(isset($product))
   <table>
     <tr>
       <th>商品名</th>
-      <td>{{ $product->product_name }}</td>
+      <td>{{$product->product_name}}</td>
     </tr>
-    <tr>
-      <th>価格</th>
-      <td>{{ $product->price }}円</td>
-    </tr>
+    <th>価格</th>
+    <td>{{$product->price}}</td>
   </table>
-
   <form action="{{ route('sessions.destroy') }}" method="POST">
     @csrf
     @method('DELETE')
     <input type="submit" value="カートを空にする">
   </form>
   @else
-  <p>カートの中身は空です。</p>
+  <p>カートの中にはからです</p>
   @endif
+  <a href="{{ route('sessions.create'}}">商品選択ページ</a>
 
-  <a href="{{ route('sessions.create') }}">商品選択ページ</a>
 </body>
 
 </html>
